127c19b4cb02d7e6273cce2a13edfb40abe34bc5
fabioazola <103256996+fabioazola@users.noreply.github.com>
2022-11-10 22:17:12 -0300

    adição -> inicio da implementação da classe macronutriente

---
+++ source/health-calc-dotnet/health-calc-pack-dotnet/Models/MacronutrienteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace health_calc_pack_dotnet.Models
+{
+    public class MacronutrienteModel
+    {
+        public int Carboidratos { get; set; }
+        public int Proteinas { get; set; }
+        public int Gorduras { get; set; }
+    }
+}
